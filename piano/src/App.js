8a524e49927b4133577b0821fb03266eac6c4122
tauan-longaretti <tauan-longaretti.com>
2020-03-10 03:12:01 -0400

    routing in place with basic nav, login and register components

--- piano/src/App.js
+++ piano/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Login from "./components/Login";
+import Register from "./components/Register";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, I am a piano!</h1>
-    </div>
+    <Router>
+      <div className="App">
+          <NavBar />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch> 
+      </div>
+    </Router>
   );
 }
 
---
+++ piano/src/components/Register.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+    Button,
+    Container,
+    Col,
+    Form,
+    FormGroup,
+    FormText,
+    Input,
+    Label
+  } from "reactstrap";
+
+const Register = () => {
+    return(
+        <div>
+            <h1>I am a Register component</h1>
+        </div>
+    )
+}
+
+export default Register;
