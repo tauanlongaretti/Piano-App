590679a0be95b04a8473f83e83a22b64b2c29343
tauan-longaretti <tauan-longaretti.com>
2020-03-10 17:29:55 -0400

    state and handleChange in place for login and register forms

--- piano/src/components/Register.js
+++ piano/src/components/Register.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
     Button,
-    Container,
-    Col,
     Form,
-    FormGroup,
-    FormText,
     Input,
-    Label,
     Row
   } from "reactstrap";
 
 const Register = () => {
+    const [newUser, setNewUser] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleChange = event => {
+        setNewUser({ ...newUser, [event.target.name]: event.target.value })
+    }
+
     return(
         <div>
             <h3>Music is only a few steps away!</h3>
@@ -21,15 +25,22 @@ const Register = () => {
                     <Input 
                     type="text"
                     name="username" 
-                    placeholder="Username" 
+                    placeholder="Username"
+                    value={newUser.username}
+                    onChange={handleChange} 
                     />
                 </Row>
                 <Row>
                     <Input 
                     type="text"
                     name="password" 
-                    placeholder="Password" 
+                    placeholder="Password"
+                    value={newUser.username}
+                    onChange={handleChange} 
                     />
+                </Row>
+                <Row>
+                    <Button>Register</Button>
                 </Row>
             </Form>
             <p>Already have an account?</p>
